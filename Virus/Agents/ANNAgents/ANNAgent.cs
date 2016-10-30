9b68c05df93d9371715ebc4d1fb65db58dc2cfb7
Armienn <nigathr@gmail.com>
2016-10-30 13:30:24 +0100

    Set up general AnnAgent structure

--- Virus/Agents/ANNAgents/ANNAgent.cs
+++ Virus/Agents/ANNAgents/ANNAgent.cs
@@ -6,23 +6,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VirusNameSpace.Agents.ANNAgents
+namespace VirusNameSpace.Agents
 {
-	public class ANNAgent : Agent
+	public class AnnAgent : Agent
 	{
-		ActivationNetwork network = new ActivationNetwork(new SigmoidFunction(), 26, 20, 20);
+		ActivationNetwork network;
+		bool learning = true;
+		MinimaxAgent teacher;
+
+		public AnnAgent(int boardSize, byte player = 1)
+		{
+			playerNumber = player;
+			int boardFields = boardSize * boardSize;
+			network = new ActivationNetwork(new SigmoidFunction(), boardFields, 30, 30, boardFields * 2);
+			teacher = new MinimaxAgent(4, player);
+		}
+
 		public override Move Move(Virus percept)
 		{
-			VirusBoard currentState = percept.GetBoardCopy();
-			Move[] actions = currentState.GetPossibleMoves(playerNumber);
-			Move action = actions[0];
-
-			return action;
+			if (learning)
+				return LearnFromMinimax(percept);
+			return GetAnnMove(percept);
 		}
 
 		public override void EndGame(Virus percept)
 		{
 
+		}
+
+		private Move LearnFromMinimax(Virus percept)
+		{
+			Move move = teacher.Move(percept);
+			//lær fra minimax
+
+			return move;
+		}
+
+		private Move GetAnnMove(Virus percept)
+		{
+			VirusBoard currentState = percept.GetBoardCopy();
+			Move[] actions = currentState.GetPossibleMoves(playerNumber);
+			Move action = actions[0];
+			return new Move();
 		}
 	}
 }
